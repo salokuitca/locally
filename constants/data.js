417d88a5a036c088f591861ddff557b51d533351
Salome Kuitca <salokuitca@gmail.com>
2021-07-01 21:36:41 -0300

    warning id solucionado

--- constants/data.js
+++ constants/data.js
@@ -3,37 +3,37 @@ export default [
     'name': 'Deco Garden',
     'description': 'Plantitas Ideales para Regalar',
     'distance': '200m',
-    'id' : 1
+    'id' : '1'
     },
     {
     'name': 'Morella',
     'description': 'Micro-Cervecería Artesanal',
     'distance': '250m',
-    'id' : 2
+    'id' : '2'
     },
     {
     'name': 'Loveland',
     'description': 'Masas Finas y otras delicias',
     'distance': '300m',
-    'id' : 3
+    'id' : '3'
     },
     {
     'name': 'Bella Viste',
     'description': 'Mallas y Bikinis',
     'distance': '400m',
-    'id' : 4
+    'id' : '4'
     },
     {
     'name': 'Black Mamba',
     'description': 'Ropa Negra de diseño',
     'distance': '500m',
-    'id' : 5
+    'id' : '5'
     },
     {
     'name': 'Deco-Decor',
     'description': 'Diseñamos tu nuevo hogar',
     'distance': '700m',
-    'id' : 6
+    'id' : '6'
     },
     
     
