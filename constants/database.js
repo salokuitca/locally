86cc9bc6d2040d6ab84fc13543ab06c081044414
Salome Kuitca <salokuitca@gmail.com>
2021-08-10 13:15:20 -0300

    Eliminadas constantes no usadas

--- constants/database.js
+++
@@ -1,3 +0,0 @@
-// export const URL_AUTH_SIGNUP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA7Vd8pFG9nngUV7O-qaPtyEJu1194Y3iY';
-
-// export const URL_AUTH_LOGIN = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA7Vd8pFG9nngUV7O-qaPtyEJu1194Y3iY';
